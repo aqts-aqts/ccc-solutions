7a1d510494132e17d056d1057acb3104f66d2a88
aqts-aqts <alvinxiao07@gmail.com>
2022-04-27 22:12:39 -0700

    ​

--- 1999/S5.py
+++ 1999/S5.py
@@ -3,20 +3,22 @@ input = sys.stdin.readline
 
 def solve(n1, n2, s, layer, nums):
     global solution
-    if layer == min(len(n1), len(n2)) - 1: solution = nums
+    if layer == min(len(n1), len(n2)): solution = nums
     for i in range(10):
+        if i == 0 and layer == len(n1) - 1: continue
         if nums[i] == None or nums[i] == n1[layer]: nums[i] = n1[layer]
         else: continue
         for j in range(10):
+            if j == 0 and layer == len(n2) - 1: continue
             if nums[j] == None or nums[j] == n2[layer]: nums[j] = n2[layer]
             else: continue
             if nums[(i + j) % 10] == s[layer] or nums[(i + j) % 10] == None:
                 nums[(i + j) % 10] = s[layer]
-                solve(layer + 1, nums)
+                solve(n1, n2, s, layer + 1, nums)
 
 for _ in range(int(input())):
     solution = [None] * 10
-    n1 = input(); n2 = input(); s = input()
+    n1 = input().strip(); n2 = input().strip(); s = input().strip()
     solve(n1, n2, s, 0, [None] * 10)
     for i in range(len(solution)):
         ss = solution[i]
